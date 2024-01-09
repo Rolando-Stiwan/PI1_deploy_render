a98b5a426e3e6b1f2168c79ce5a3a96a2a24c962
Rolando-Stiwan <rolandostiwan@gmail.com>
2024-01-09 11:58:25 -0500

    Se_actualizo_el_main

--- main.py
+++ main.py
@@ -32,8 +32,12 @@ def index():
 def PlayTimeGenre(genre: str= Query(..., 
                                 description="Genero de juego con mas horas jugadas", 
                                 example="Action")):
-    genre = genre.capitalize()
-    filtered_data = playtime[playtime['genres'] == genre]
+    genre = genre.lower()
+    filtered_data = playtime[playtime['genres'].str.lower() == genre]
+    
+    if filtered_data.empty:
+        return {"No hay datos para el género."}
+
     grouped = filtered_data.groupby('rel_year')['playtime_forever'].sum().reset_index()
     año_mas_jugado = grouped.loc[grouped['playtime_forever'].idxmax(), 'rel_year']
     
@@ -45,13 +49,13 @@ def PlayTimeGenre(genre: str= Query(...,
 def UserForGenre(genre: str= Query(..., 
                                 description="Usuario con mas horas de juego en un genero", 
                                 example="Action")):
-    genre = genre.capitalize()
+    genre = genre.lower()
     # Filtrar el DataFrame por el género dado
-    genero = userfor[userfor['genres'] == genre]
+    genero = userfor[userfor['genres'].str.lower() == genre]
     
     if genero.empty:
-        print(f"No hay datos para el género '{genre}'.")
-        return
+        #print(f"No hay datos para el género '{genre}'.")
+        return {"No hay datos para el género."}
     
     # Encontrar el usuario que más horas ha jugado ese género
     max_user = genero.loc[genero['playtime_forever'].idxmax()]['user_id']
@@ -77,6 +81,9 @@ def UsersRecommend(año: int= Query(...,
                                 example=2011)):
     # Filtrar el DataFrame por el año proporcionado
     filtro = user_recom[user_recom['año'] == año]
+
+    if filtro.empty:
+        return {"No hay datos para el año."}
     
     # Obtener los nombres de app_name en el mismo orden que aparecen en el DataFrame original
     nombres_app = filtro['app_name'].tolist()
@@ -92,6 +99,9 @@ def UsersWorstDeveloper(año: int= Query(...,
                                 example=2012)):
     # Filtrar el DataFrame por el año proporcionado
     filtro = user_worst[user_worst['año'] == año]
+
+    if filtro.empty:
+        return {"No hay datos para el año."}
     
     # Obtener los nombres de app_name en el mismo orden que aparecen en el DataFrame original
     developer = filtro['developer'].tolist()
@@ -106,7 +116,12 @@ def UsersWorstDeveloper(año: int= Query(...,
 def sentiment_analysis(developer: str= Query(..., 
                                 description="Dada la empresa desarrolladora se muestra la cantidad de reseñas de usuarios, categorizadas", 
                                 example="Gaijin Games")):
-    filtro = sa.loc[sa['developer'] == developer]
+    developer = developer.lower()
+    filtro = sa.loc[sa['developer'].str.lower() == developer]
+
+    if filtro.empty:
+        return {"No hay datos para el desarrollador."}
+    
     resultado = {
         developer: {
             'Negative': int(filtro.iloc[0, 1]), 
@@ -121,15 +136,19 @@ def sentiment_analysis(developer: str= Query(...,
 def recomendacion_juego(game: str= Query(..., 
                                 description="Dado el id del juego se recibe una lista de 5 juegos similares al ingresado", 
                                 example="10")):
-    recommendations = []
+    recomendacion = []
     count = 1
-    #game_str = str(game)  # Convertir el valor numérico a cadena
-    print('Similar games to {} include:\n'.format(game))
-    for item in df_item_cos.sort_values(by=game, ascending=False).index[1:6]:
-        recommendations.append({'No.': count, 'Juego': str(item)})
+    if game not in df_item_cos.columns:
+        return {"Mensaje": f"El juego con el ID {game} no está en el listado de juegos."}
+    
+    print('Juegos similares a {} include:\n'.format(game))
+    juegos_similares = df_item_cos.sort_values(by=game, ascending=False).index[1:6]
+
+    for item in juegos_similares:
+        recomendacion.append({'No.': count, 'Juego': str(item)})
         count += 1
     
-    return {"Juegos similares a {}".format(game): recommendations}
+    return {"Juegos similares a {}".format(game): recomendacion}
 
 
 
@@ -137,27 +156,33 @@ def recomendacion_juego(game: str= Query(...,
 def recomendacion_usuario(user_id: str= Query(..., 
                                 description="Dado el user_id se recopmienda 5 juegos al usuario", 
                                 example="OfficialShroomsy")):
+    user_id = user_id.lower()
     n=5
-    # Encuentra el índice del usuario en el DataFrame
-    user_index = training_2[training_2['user_id'] == user_id]['user_id_encoded'].iloc[0]
+
+    # Verificar si el user_id está en el DataFrame
+    if user_id not in training_2['user_id'].str.lower().unique():
+        return {"Mensaje": f"El usuario con el ID {user_id} no está registrado en los datos."}
     
-    # Obtener juegos similares ordenados por similitud coseno
-    similar_games = list(enumerate(cosine_sim[user_index]))
-    similar_games = sorted(similar_games, key=lambda x: x[1], reverse=True)
+    # Encuentra el índice del usuario en el DataFrame
+    user_index = training_2[training_2['user_id'].str.lower() == user_id]['user_id_encoded'].iloc[0]
+    
+    # Obtener juegos similares ordenados por similitud de coseno
+    juegos_similares = list(enumerate(cosine_sim[user_index]))
+    juegos_similares = sorted(juegos_similares, key=lambda x: x[1], reverse=True)
     
     # Obtener los juegos ya jugados por el usuario
-    games_played = set(training_2[training_2['user_id'] == user_id]['item_id'].tolist())
+    juegos_jugados = set(training_2[training_2['user_id'].str.lower() == user_id]['item_id'].tolist())
     
     # Filtrar juegos similares que ya han sido jugados y no están en la lista
-    top_similar_games = []
-    for idx, _ in similar_games:
-        if len(top_similar_games) >= n:
+    top_juegos_similares = []
+    for idx, _ in juegos_similares:
+        if len(top_juegos_similares) >= n:
             break
         game_id = training_2.iloc[idx]['item_id']
-        if game_id not in games_played and game_id not in top_similar_games:
-            top_similar_games.append(game_id)
+        if game_id not in juegos_jugados and game_id not in top_juegos_similares:
+            top_juegos_similares.append(game_id)
     # Crear el diccionario con el formato deseado
-    recommended_games = [{'No.': i+1, 'Juego': str(game_id)} for i, game_id in enumerate(top_similar_games)]
-    output = {'Juegos recomendados a {}'.format(user_id): recommended_games}
+    juegos_recomendados = [{'No.': i+1, 'Juego': str(game_id)} for i, game_id in enumerate(top_juegos_similares)]
+    salida = {'Juegos recomendados a {}'.format(user_id): juegos_recomendados}
     
-    return output
+    return salida
